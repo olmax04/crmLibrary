76ffcea0557e6727a7664ec9b6f706af25179640
Olmax <olmax04developer@gmail.com>
2023-10-15 03:16:32 +0200

    ver 1.0.0, upd: Preload[Images]

---
+++ src/crmLibrary_olmax04/preload.py
@@ -0,0 +1,20 @@
+from src.crmLibrary_olmax04.rieltorModule import RieltorModule
+
+
+class Preload:
+    @staticmethod
+    def preload_handler(list):
+        preload_data = {}
+        for i, file in enumerate(list):
+            preload_data[f"file{i}"] = file
+        return preload_data
+
+    @staticmethod
+    @RieltorModule.request
+    def create(files_urls) -> dict:
+        data = dict()
+        data['url'] = "preloads/object_photo/"
+        data['headers'] = {'Accept': "*/*"}
+        data['method'] = "POST"
+        data['files'] = Preload.preload_handler(files_urls)
+        return data
